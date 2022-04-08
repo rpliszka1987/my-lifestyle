a7b2338bca67728074850aa241ef11607c893f99
Alex Vazquez <avazque008@outlook.com>
2022-04-08 17:17:50 -0400

    store and retrieve tasks from localStorage

--- assets/js/tasks.js
+++ assets/js/tasks.js
@@ -1,7 +1,54 @@
+var taskList = [];
+
+
+$(".save-btn").on("click", function(e) {
+    e.preventDefault()
+    var parent = $(this).closest(".modal");
+    var text = parent.children(".textarea").val().trim();
+    const newTask = {
+        task_text: text
+    }
+    saveTaskItem(newTask)
+    createTaskItem(newTask);
+});
+
+var createTaskItem = function(task) {
+    var list = $("<li>").addClass("columns task-list-item");
+    var checkDiv = $("<div>").addClass("column is-2 check-box").html("<button class='btn-check check-no-color'><i class='fa-solid fa-check fa-lg'></i></button>");
+    var taskDiv = $("<div>").addClass("column is-10 task-box").html("<p class='task'>" + task.task_text + "</p>");
+
+    list.append(checkDiv);
+    list.append(taskDiv);
+    $(".tasks-list").append(list);
+};
+
+var saveTaskItem = function(task) {
+    taskList.push(task)
+    console.log(taskList)
+    localStorage.setItem('taskStorage',JSON.stringify(taskList))
+};
+
+function getStoredTasks(){
+    var storedtasks = localStorage.getItem('taskStorage')
+    if(storedtasks){
+        taskList = JSON.parse(storedtasks);
+    }
+    console.log(taskList)
+    taskList.forEach(task => {
+        createTaskItem(task)
+    });
+};
+
+getStoredTasks();
+
+// Clear textarea after modale closes
+$(".btn-create").on("click", function (){
+    $("textarea").val("");
+});
 
 
 // Display a green check when checkbox is clicked
-$(".btn-check").on("click", function(){
+$("ul").on("click", ".btn-check", function(){
     $(this).removeClass("check-no-color").addClass("check-color");
     var taskIndex = $(this).closest(".task-list-item").index();
     console.log(taskIndex);
@@ -10,21 +57,6 @@ $(".btn-check").on("click", function(){
     console.log(taskText);
 
     localStorage.setItem(taskIndex, JSON.stringify(taskText));    
-});
-
-// Get the task from modal
-$(".save-btn").on("click", function() {
-    var parent = $(this).closest(".modal");
-    var text = parent.children(".textarea").val();
-    console.log(text);
-});
-
-
-
-
-// Clear textarea after modale closes
-$(".btn-create").on("click", function (){
-    $(".textarea").val("");
 });
 
 
