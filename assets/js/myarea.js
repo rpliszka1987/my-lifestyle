5ccfeca56bdbf8d62ff76ba1a992ed9bb75051b2
Spencer Jagernauth <rita.jagernauth01@gmail.com>
2022-04-11 19:38:49 -0400

    added new js

--- assets/js/myarea.js
+++ assets/js/myarea.js
@@ -1,13 +1,20 @@
 //This initializes the pop-up for the location
 //Adding a map to locate local restaurants near me
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places"> 
-//var map
-//var infowindow
 function initMap() {
     const map = new google.maps.Map(document.getElementById ("map"))
-    const Flushing = new google.maps.LatLng (-73.833,40.768)
+    var location = {lat: -73.8331, lng: 40.7675);
+    var map = map = new google.maps.Map(
+        document.getElementById("map"), 
+        {center: location 
+            zoom: 8});
 
     const request = {
         placeId: "ChIJP2PATQVgwokRHih0tNEk7Po"
     }
+    infowindow = new google.maps.InfoWindow();
+
+    map = new google.maps.Map(
+        document.getElementById('map'), 
+
 }
