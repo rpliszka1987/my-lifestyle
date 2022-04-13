a0397c7c799980012f895889b3f3a652e0737dbf
Spencer Jagernauth <rita.jagernauth01@gmail.com>
2022-04-13 16:46:02 -0400

    added maps

--- assets/js/myarea.js
+++ assets/js/myarea.js
@@ -1,25 +1,57 @@
 //This initializes the pop-up for the location
-  //Adding a map to locate local restaurants near me 
-  function initMap() {
-      var map = new google.maps.Map(document.getElementById ("map"))
-      var location = {lat: -73.8331, lng: 40.7675
-          _const: map = new google.maps.Map(
-              document.getElementById("map")), 
-      get const() {
-          return this._const;
-      },
-      set const(value) {
-          this._const = value;
-      },
-          {center: location 
-              zoom: 8
-              };
+//Adding a map to locate local restaurants near me
+
+function initMap() {
+    const location = {lat: -73.8331, lng: 40.7675
+    var map = new google.maps.Map(document.getElementById ("map"))
+         {center: location 
+            zoom: 8
+            mapId:c84822eab8f75974
+            };
+        }
+        var input = document.getElementById('search input');
+        map.controls[google.maps.ControlPostion.TOP_LEFT].push(input);
+ 
+ 
+        const service = new google.maps.places.PlacesService(map);
+        var getNextPage;
+        const moreButton = document.getElementById("more");
+      
+        moreButton.onclick = function () {
+          moreButton.disabled = true;
+          if (getNextPage) {
+            getNextPage();
           }
-      const request = {
-          placeId: "ChIJP2PATQVgwokRHih0tNEk7Po"
-      }
-      infowindow = new google.maps.InfoWindow();
-  
-      map = new google.maps.Map(
-          document.getElementById('map'));
-          
+        };
+//Adding a list to get the set of places around me on the map
+      function addPlaces (places,map)
+      const placesList = document.getElementById("places");
+      const placesList = document.getElementById("places");
+
+  for (const place of places) {
+    if (place.geometry && place.geometry.location) {
+      const image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+
+      new google.maps.Marker({
+        map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location,
+      });
+
+      const li = document.createElement("li");
+
+      li.textContent = place.name;
+      placesList.appendChild(li);
+      li.addEventListener("click", () => {
+        map.setCenter(place.geometry.location);
+      });
+    }
+  }
+}
