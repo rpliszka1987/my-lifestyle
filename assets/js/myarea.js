30dcaa40d4ef265d60d37fd886349ec2636e220d
Spencer Jagernauth <rita.jagernauth01@gmail.com>
2022-04-13 15:29:34 -0400

    added js

--- assets/js/myarea.js
+++ assets/js/myarea.js
@@ -1,20 +1,25 @@
 //This initializes the pop-up for the location
-//Adding a map to locate local restaurants near me
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places"> 
-function initMap() {
-    const map = new google.maps.Map(document.getElementById ("map"))
-    var location = {lat: -73.8331, lng: 40.7675);
-    var map = map = new google.maps.Map(
-        document.getElementById("map"), 
-        {center: location 
-            zoom: 8});
-
-    const request = {
-        placeId: "ChIJP2PATQVgwokRHih0tNEk7Po"
-    }
-    infowindow = new google.maps.InfoWindow();
-
-    map = new google.maps.Map(
-        document.getElementById('map'), 
-
-}
+  //Adding a map to locate local restaurants near me 
+  function initMap() {
+      var map = new google.maps.Map(document.getElementById ("map"))
+      var location = {lat: -73.8331, lng: 40.7675
+          _const: map = new google.maps.Map(
+              document.getElementById("map")), 
+      get const() {
+          return this._const;
+      },
+      set const(value) {
+          this._const = value;
+      },
+          {center: location 
+              zoom: 8
+              };
+          }
+      const request = {
+          placeId: "ChIJP2PATQVgwokRHih0tNEk7Po"
+      }
+      infowindow = new google.maps.InfoWindow();
+  
+      map = new google.maps.Map(
+          document.getElementById('map'));
+          
