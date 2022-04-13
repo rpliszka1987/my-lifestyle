039d83fcc34bc47cdde61565806a83314587a3da
Chris Mullendore <chris@vindage.com>
2022-04-13 08:17:31 -0700

    Changed dynamic loading to ./ vs ../

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,8 +15,8 @@ function LoadContent(path, target) {
     })
 }
 
-LoadContent("../content/contacts.html", "#contacts-content");
-LoadContent("../content/tasks.html", "#tasks-content");
-LoadContent("../content/shoppinglist.html", "#shopping-list-content");
-LoadContent("../content/weather.html", "#weather-content");
-LoadContent("../content/myarea.html", "#my-area-content");
+LoadContent("./content/contacts.html", "#contacts-content");
+LoadContent("./content/tasks.html", "#tasks-content");
+LoadContent("./content/shoppinglist.html", "#shopping-list-content");
+LoadContent("./content/weather.html", "#weather-content");
+LoadContent("./content/myarea.html", "#my-area-content");
